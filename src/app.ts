68b0cf466ecf22267c7ce13be9daf35d701c3b22
Leonardo Araujo <mendes.lnr@gmail.com>
2022-06-24 21:15:21 -0300

    Adding CORS

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import compression from 'compression';
+import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
@@ -12,6 +13,8 @@ const port = process.env.PORT || '3000';
 const mongoUri = process.env.MONGO_URI || '';
 
 const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 

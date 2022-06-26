9ad77f7e9acdacd4fdd124ebcbf0893221a8e1e4
Leonardo Araujo <mendes.lnr@gmail.com>
2022-06-26 16:25:18 -0300

    dist cleanup

--- dist/middlewares/notfound.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundMiddleware = (req, res, next) => {
-    res.status(404).json({});
-};
-exports.default = notFoundMiddleware;
